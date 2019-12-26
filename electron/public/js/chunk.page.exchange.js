e633ff114896856d8b4742b98455bcc789d0530e
VoroninV <paxexterminatus@gmail.com>
2019-12-26 14:05:07 +0300

    rebuild

---
+++ electron/public/js/chunk.page.exchange.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk.page.exchange"],{"5c44":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"page exchange"},[n("h1",{staticClass:"title h1"},[e._v("Exchange")])])}],s={},l=s,i=n("2877"),r=Object(i["a"])(l,a,c,!1,null,null,null);t["default"]=r.exports}}]);
+//# sourceMappingURL=chunk.page.exchange.js.map
