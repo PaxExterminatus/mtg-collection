e4c417456cc71864b25b4c037f6307821c640eb9
VoroninV <paxexterminatus@gmail.com>
2020-01-29 18:38:32 +0300

    added db sqlite

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { ApiCollectionController } from './api.collection.controller'
 import { ApiCollectionService } from './api.collection.service'
 import { join } from 'path'
+import { Connection } from 'typeorm'
 
 @Module({
     imports: [
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '..', 'public')
         }),
+        TypeOrmModule.forRoot({
+            type: 'sqlite',
+            database: join(__dirname, '..', 'db.db')
+        }),
     ],
     controllers: [ApiCollectionController],
     providers: [ApiCollectionService]
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private readonly connection: Connection) {
+        console.log(this.connection);
+    }
+}
